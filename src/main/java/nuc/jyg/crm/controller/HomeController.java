564fcacfcf60ae44cdd659e8c93a5039cf561666
Aolish333 <aolish333@gmail.com>
2018-09-06 16:27:33 +0800

    :construction:

--- src/main/java/nuc/jyg/crm/controller/HomeController.java
+++ src/main/java/nuc/jyg/crm/controller/HomeController.java
@@ -24,4 +24,36 @@ public class HomeController {
     public String sale() {
         return "sales-opportunity";
     }
+
+    @GetMapping(value = "/developingCustomer")
+    public String developingCustomer() {
+        return "customer-develop";
+    }
+
+    @GetMapping(value = "/createService")
+    public String createService() {
+        return "service-create";
+    }
+
+    @GetMapping(value = "/serviceDistribution")
+    public String serviceDistribution() {
+        return "service-distribution";
+    }
+
+    @GetMapping(value = "/serviceProcess")
+    public String serviceProcess() {
+        return "service-handle";
+    }
+
+
+    @GetMapping(value = "/serviceFeedback")
+    public String serviceFeedback() {
+        return "service-feedback";
+    }
+
+    @GetMapping(value = "/serviceArchive")
+    public String archiveServices() {
+        return "service-file";
+    }
+
 }
